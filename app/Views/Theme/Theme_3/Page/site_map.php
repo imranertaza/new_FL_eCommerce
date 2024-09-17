30c8efce999afe9d2fe56c53683c9fa6246c9feb
imranertaza <imranertaza12@gmail.com>
2024-09-17 20:00:37 +0600

    sitemap new update Co-authored-by: Murad <dnationsoftdm8@gmail.com>

---
+++ app/Views/Theme/Theme_3/Page/site_map.php
@@ -0,0 +1,32 @@
+<section class="main-container my-0">
+    <div class="container">
+        <div class="contact-form bg-white  p-4 p-lg-5 mb-5 mt-5">
+            <div class="row">
+                <div class="col-lg-12 ">
+                    <h3 class="text-capitalize mb-4"><?php echo $pageData->page_title;?></h3>
+                    <?php //echo $pageData->page_description;?>
+                    <?php
+                        $xml = simplexml_load_file($_SERVER['DOCUMENT_ROOT'] . "/sitemap.xml");
+
+                        echo "<ul>";
+                        foreach ($xml->page as $val){
+                            echo "<li> ";
+                            if ($val->parent == '0'){
+                                echo "<a href=".$val->url.">".htmlspecialchars($val->title)."</a>";
+                            }
+                            echo "<ul>";
+                            if ($val->parent == '1'){
+                                echo "<li><div><a href=".$val->url.">".htmlspecialchars($val->title)."</a></div></li>";
+                            }
+                             echo "</ul></li>";
+
+                        }
+                        echo "</ul>";
+                    ?>
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
