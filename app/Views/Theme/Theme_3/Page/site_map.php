0437ebdb952b66e5c43752c9c9c824f1f68a6873
dnationsoft12345 <dnationsoftdm8@gmail.com>
2024-11-20 17:14:01 +0600

    Mouse paste not working on search option in admin panel products page. Co-authored-by: Murad <dnationsoftdm8@gmail.com>

--- app/Views/Theme/Theme_3/Page/site_map.php
+++ app/Views/Theme/Theme_3/Page/site_map.php
@@ -1,6 +1,6 @@
 <section class="main-container my-0">
     <div class="container">
-        <div class="contact-form bg-white  p-4 p-lg-5 mb-5 mt-5">
+        <div class="contact-form bg-white  py-4 mb-5 mt-5">
             <div class="row">
                 <div class="col-lg-12 ">
                     <h3 class="text-capitalize mb-4"><?php echo $pageData->page_title;?></h3>
