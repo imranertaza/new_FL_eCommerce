08871e7de80646aa442af96bb8dfe6a788f00e73
dnationsoft12345 <dnationsoftdm8@gmail.com>
2025-04-24 16:28:40 +0600

    offer comment back

--- app/Views/Admin/sidebar.php
+++ app/Views/Admin/sidebar.php
@@ -121,16 +121,16 @@
                 <?php } ?>
 
 
-<!--                <li class="nav-item">-->
-<!--                    <a href="#" class="nav-link">-->
-<!--                        <i class="nav-icon fas fa-copy"></i>-->
-<!--                        <p> Offer <i class="fas fa-angle-left right"></i>-->
-<!--                        </p>-->
-<!--                    </a>-->
-<!--                    <ul class="nav nav-treeview">-->
-<!--                        --><?php //echo add_main_based_menu_with_permission('General Offer', base_url('general_offer'), $adRoleId, 'fa-circle', 'General_offer'); ?>
-<!--                    </ul>-->
-<!--                </li>-->
+                <li class="nav-item">
+                    <a href="#" class="nav-link">
+                        <i class="nav-icon fas fa-copy"></i>
+                        <p> Offer <i class="fas fa-angle-left right"></i>
+                        </p>
+                    </a>
+                    <ul class="nav nav-treeview">
+                        <?php echo add_main_based_menu_with_permission('General Offer', base_url('general_offer'), $adRoleId, 'fa-circle', 'General_offer'); ?>
+                    </ul>
+                </li>
 
 
                 <?php
