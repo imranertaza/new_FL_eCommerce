f327574d8610f4ee1a75af65e668013951a3e61f
dnationsoft12345 <dnationsoftdm8@gmail.com>
2025-12-24 17:09:01 +0600

    w3 validation

--- app/Views/Theme/Theme_3/layout.php
+++ app/Views/Theme/Theme_3/layout.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo $title;?></title>
+    <title><?php echo !empty($title)?$title:'ccart';?></title>
     <meta name="description" content="<?php echo $description;?>">
     <meta name="keywords" content="<?php echo $keywords;?>">
 
