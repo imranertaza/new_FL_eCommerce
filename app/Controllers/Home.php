e1e65d72fb23b653a70f34ccfa59569de989228d
dnationsoft12345 <dnationsoftdm8@gmail.com>
2026-05-12 10:14:56 +0600

    maintenance

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -244,5 +244,21 @@ class Home extends BaseController
 
     }
 
+    public function index_maintenance()
+    {
+        print "<!DOCTYPE html>
+                <title>Site Maintenance</title>
+                <style>
+                                  body { text-align: center; padding: 150px; }
+                                  h1 { font-size: 50px; }
+                                  body { font: 20px Helvetica, sans-serif; color: #333; }
+                </style>
+                <body>
+                <h1>We’ll be back soon!</h1>
+                <div><p>Sorry for the inconvenience, but we’re performing some maintenance.</p></div>
+                </body>";
+    }
+
+
 
 }
